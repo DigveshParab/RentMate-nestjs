e7779f956ef0343a514057834dc9f4244b262681
Diggi19 <digveshparab123@gmail.com>
2025-06-03 16:59:30 +0530

    feat(db): add Prisma module and DB health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly Prisma:PrismaService
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("healthcheck")
+  async healthCheck(){
+    const result = await this.Prisma.vehicle.findFirst();
+
+    return {
+      dbStatus: result ? 'Connected and data found' : 'Connected but table empty',
+    }
   }
 }
